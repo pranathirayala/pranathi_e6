22e3867215ac1e9bcf7198b2416e5e901372757c
pranathirayala <32014886+pranathirayala@users.noreply.github.com>
2017-09-16 18:09:06 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+	Student[] s=new Student(1,rayala,1967-05-19,97);
+		StudentGroup g=new StudentGroup;
+			g.getCurrentAgeByDate(1);
+	
 	}
 
 }
