08d4551edcc7fcd8fa1bced9bbb6ff625fb41811
pranathirayala <32014886+pranathirayala@users.noreply.github.com>
2017-09-16 18:23:44 +0530

    no errot2

---
+++ src/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	Student[] s=new Student[2];
+	s[0].setId(1);
+	s[0].setFullName("rrrr");
+	s[0].setBirthDate(1992-09-13);
+	s[0].setAvgMark(99);
+	s[1].setId(2);
+	s[1].setFullName("aaar");
+	s[1].setBirthDate(1992-09-14);
+	s[1].setAvgMark(96);
+
+		StudentGroup g=new StudentGroup();
+			g.getCurrentAgeByDate(1);
+	
+	}
+
+}
